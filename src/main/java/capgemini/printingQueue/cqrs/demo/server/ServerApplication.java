973503f656a4d56ce193a18d6bcbe2edcaf6c3b6
Maciej Maciejewski <maciej.maciejewski@capgemini.com>
2019-02-15 13:51:25 +0100

    added http basic auth with mocked user and admin

--- src/main/java/capgemini/printingQueue/cqrs/demo/server/ServerApplication.java
+++ src/main/java/capgemini/printingQueue/cqrs/demo/server/ServerApplication.java
@@ -2,8 +2,10 @@ package capgemini.printingQueue.cqrs.demo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("capgemini.printingQueue.cqrs.demo")
 public class ServerApplication {
 
 	public static void main(String[] args) {
