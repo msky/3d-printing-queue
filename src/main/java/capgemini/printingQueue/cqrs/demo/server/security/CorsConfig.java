46b8cc7ae983e3350eef19677ddfeac896ddb356
Maciej Maciejewski <maciej.maciejewski@capgemini.com>
2019-02-16 15:25:23 +0100

    added missing class with CORS config

--- src/main/java/capgemini/printingQueue/cqrs/demo/server/security/CorsConfig.java
+++ src/main/java/capgemini/printingQueue/cqrs/demo/server/security/CorsConfig.java
@@ -1,18 +1,23 @@
 package capgemini.printingQueue.cqrs.demo.server.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+	@Autowired
+	Environment env;
+	
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://CE11846:4200")
+                        env.getProperty("cors.client.origin"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true)
         ;
